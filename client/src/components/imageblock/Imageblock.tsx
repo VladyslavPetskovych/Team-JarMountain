3ae74d2f29e78b04bafe15be190b94758f26135a
Vlad <peckovychv@gmail.com>
2022-11-19 13:37:55 +0200

    Added Routing and minor frontend improvements

---
+++ client/src/components/imageblock/Imageblock.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Imageblock() {
+  return (
+    <div>Imageblock</div>
+  )
+}
+
+export default Imageblock
---
+++ client/src/pages/Payment.tsx
@@ -0,0 +1,3 @@
+export default function Payment(){
+    return <h1>PAYMENT!!!!!!!</h1>
+}
